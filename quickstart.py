8356f6660406c82cf08249f297f1302b979e362b
AdegokeMichael <michaelbeatus.mia@gmail.com>
2025-08-22 12:32:20 +0100

    create quickstart file to run app

---
+++ quickstart.py
@@ -0,0 +1,18 @@
+from ai_clips_maker import Transcriber, ClipFinder, resize
+
+# Step 1: Transcription
+transcriber = Transcriber()
+transcription = transcriber.transcribe(audio_file_path="/home/michael_adegoke/testvideo.mp4")
+
+# Step 2: Clip detection
+clip_finder = ClipFinder()
+clips = clip_finder.find_clips(transcription=transcription)
+print(clips[0].start_time, clips[0].end_time)
+
+# Step 3: Cropping & resizing
+crops = resize(
+    video_file_path="/home/michael_adegoke/testvideo.mp4",
+    #pyannote_auth_token="your_huggingface_token",
+    aspect_ratio=(9, 16)
+)
+print(crops.segments)
