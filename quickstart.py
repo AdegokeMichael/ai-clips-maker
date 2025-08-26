f6af08d6eb87a833a191dfa3fe0fb9bbf3fbfa34
AdegokeMichael <michaelbeatus.mia@gmail.com>
2025-08-26 13:21:58 +0100

    changed video extension for testvideo

--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,7 @@ from ai_clips_maker import Transcriber, ClipFinder, resize
 
 # Step 1: Transcription
 transcriber = Transcriber()
-transcription = transcriber.transcribe(audio_path="/home/michael_adegoke/testvideo.mp4", lang= "en")
+transcription = transcriber.transcribe(audio_path="/home/michael_adegoke/testvideo.webm", lang= "en")
 
 # Step 2: Clip detection
 clip_finder = ClipFinder()
@@ -11,7 +11,7 @@ print(clips[0].start_time, clips[0].end_time)
 
 # Step 3: Cropping & resizing
 crops = resize(
-    video_file_path="/home/michael_adegoke/testvideo.mp4",
+    video_file_path="/home/michael_adegoke/testvideo.webm",
     #pyannote_auth_token="your_huggingface_token",
     aspect_ratio=(9, 16)
 )
